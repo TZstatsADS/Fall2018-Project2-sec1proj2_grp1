a1dc2b56a0d8d88093ca089e7bae2a9e254582ed
Yunfan Li <yl3838@columbia.edu>
2018-10-03 19:45:49 -0400

    googleway

---
+++ doc/googleway.R
@@ -0,0 +1,43 @@
+## using split view
+library(shinydashboard)
+library(googleway)
+ui <- dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody(
+    box(width = 6,
+        google_mapOutput(outputId = "map")
+    ),
+    box(width = 6,
+        google_mapOutput(outputId = "pano")
+    )
+  ) )
+server <- function(input, output) {
+  set_key(key)
+  output$map <- renderGoogle_map({
+    res <- google_directions(origin = "Flinders Street Station, Melbourne",
+                             destination = "MCG, Melbourne",
+                             mode = "bicycling")
+    df_route <- data.frame(route = res$routes$overview_polyline$points)
+    google_map(location = c(-37.817386, 144.967463),
+               zoom = 10,
+               split_view = "pano")%>%
+      #add_bicycling()%>%
+      add_polylines(data = df_route, 
+                    polyline = "polyline",
+                    stroke_colour = "#FF33D6",
+                    stroke_weight = 7,
+                    stroke_opacity = 0.7,
+                    #info_window = "New route",
+                    load_interval = 100)#%>%
+    
+    
+    #google_map_directions(origin = "Melbourne Cricket Ground",
+    #destination = "Flinders Street Station",
+    #dir_action = "navigate")
+    #google_directions(origin = "Flinders Street Station, Melbourne",
+    #                 destination = "MCG, Melbourne",
+    #               mode = "walking")
+  }) }
+shinyApp(ui, server)
+## End(Not run)
