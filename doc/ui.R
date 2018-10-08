8c4814e5ebf9e1d72818187e78f03aa4a488a9cf
amon <amon12171993@gmail.com>
2018-10-08 17:47:22 -0400

    Update

--- doc/ui.R
+++ doc/ui.R
@@ -3,47 +3,48 @@ library(shinydashboard)
 library(leaflet)
 library(DT)
 
+
 ui <- dashboardPage(
   dashboardHeader(title = "City Bike Maps"),
   dashboardSidebar(
     sidebarMenu(
       menuItem("Maps",tabName = "tMaps",icon = icon("globe")),
-               
+
       menuItem("Data",
                icon = icon("table"),
                menuSubItem("live", tabName = "tLive"),
                menuSubItem("stations",tabName = "tStations"),
                menuSubItem("crime", tabName = "tCrime")),
-      
+
       menuItem("Contact Us",tabName= "tContact",icon = icon("envelope"))
     )
       ),
   dashboardBody(
     tabItems(
       tabItem(tabName ="tMaps",
-              h2("City Bike Maps with Weather Rador"),
-              h4("Type in your location and destination."),
-              leafletOutput("map")
+              #h2("City Bike Maps with Weather Rador"),
+              #h4("Type in your location and destination."),
+              leafletOutput("map",width=50,height=50)
               ),
-      
+
       tabItem(tabName = "tLive",
               h4("Citybikenyc.com: "),
               h4("https://www.citibikenyc.com/system-data"),
               h4("FileURL: "),
               h4("https://gbfs.citibikenyc.com/gbfs/en/station_status.json")
               ),
-     
+
        tabItem(tabName = "tStations",
               h4("Citybikenyc.com: "),
               h4("https://www.citibikenyc.com/system-data"),
               h4("FileURL: "),
               h4("https://gbfs.citibikenyc.com/gbfs/en/station_information.json")
       ),
-     
+
        tabItem(tabName = "tCrime",
               fluidRow(column(12, DT::dataTableOutput("tableCrime")))
       ),
-      
+
       tabItem("tContact",
               h2("Contact Us"),
               h4("We are group 1!"),
