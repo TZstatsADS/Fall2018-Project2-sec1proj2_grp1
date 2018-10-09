fb5765552e5f4beb936c41ade8059688273c0bd7
Yunfan Li <yl3838@columbia.edu>
2018-10-09 10:21:40 -0400

    draggable textInput

--- doc/ui.R
+++ doc/ui.R
@@ -34,14 +34,21 @@ ui <-
                 # h2("City Bike Maps with Weather Rador"),
                 # h4("Type in your location and destination."),
                 tags$style(type = "text/css", "#map {height: calc(100vh - 80px) !important;}"),
-                leafletOutput("map")
+                leafletOutput("map"),
+                # absolutePanel(
+                #   bottom = 20, right = 20, width = 300,
+                #   draggable = TRUE,
+                #   textInput(inputID = "current", label = h3('Current Location')),
+                #   textInput(inputID = "final", label = h3('Destination')),
+                #   submitButton(text = "Submit"))
                 ),
         
         tabItem(tabName = "tLive",
-                h4("Citybikenyc.com: "),
-                h4("https://www.citibikenyc.com/system-data"),
-                h4("FileURL: "),
-                h4("https://gbfs.citibikenyc.com/gbfs/en/station_status.json")
+                # h4("Citybikenyc.com: "),
+                # h4("https://www.citibikenyc.com/system-data"),
+                # h4("FileURL: "),
+                # h4("https://gbfs.citibikenyc.com/gbfs/en/station_status.json")
+                fluidRow(column(12,DT::dataTableOutput("tableLive")))
                 ),
        
          tabItem(tabName = "tStations",
